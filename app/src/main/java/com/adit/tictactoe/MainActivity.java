6e2b57b59e96bcc942b2ee3e90cdd4105ab921c7
Kevin F. Konrad <kevinkonrad@ymail.com>
2021-10-02 10:08:50 +0200

    fix array declaration style in MainActivity.java

--- app/src/main/java/com/adit/tictactoe/MainActivity.java
+++ app/src/main/java/com/adit/tictactoe/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState={2,2,2,2,2,2,2,2,2};
     // State meanings:
     // 0-X      1-O      2-Null
-    int winPositions[][]={{0,1,2},{3,4,5},{6,7,8},
+    int[][] winPositions={{0,1,2},{3,4,5},{6,7,8},
                           {0,3,6},{1,4,7},{2,5,8},
                           {0,4,8},{2,4,6}};
     public void playerTap(View view)
