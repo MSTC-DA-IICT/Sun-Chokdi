eb3bc77a951326febbf067fd667215049a22b14e
Nilay Pandya <nilay.pandya3@gmail.com>
2021-10-08 16:24:40 +0530

    Change in UI
    Here I have added a Splash Screen for Title.... as it was not looking good on main screen....
    
    I made some changes in UI of whole application.
    also added Reset Button....
    
    Added Some Toast Messages and Player Winning Status too....

--- app/src/main/java/com/adit/tictactoe/MainActivity.java
+++ app/src/main/java/com/adit/tictactoe/MainActivity.java
@@ -1,138 +1,141 @@
 package com.adit.tictactoe;
 
-import androidx.annotation.DrawableRes;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Arrays;
-import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.PersistableBundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.adit.tictactoe.dialogs.SelectPlayerDialog;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private TextView playeroneScore, playerTwoScore, playerStatus;
+    private Button[] buttons = new Button[9];
+    private Button resetGame;
 
+    private int playerOneScoreCount, playerTwoScoreCount, rountCount;
+    boolean activePlayer;
 
-import java.util.Arrays;
-public class MainActivity extends AppCompatActivity {
-    SelectPlayerDialog dialog;
-    // Player Representation:
-    //0 - X
-    //1 - O
-    static public int activePlayer=-1;
-    boolean gameActive=true;
-    int[] gameState={2,2,2,2,2,2,2,2,2};
-    // State meanings:
-    // 0-X      1-O      2-Null
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, //Rows
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, //Cols
+            {0, 4, 8}, {2, 4, 6} //Cross
+    };
 
-    int[][] winPositions={{0,1,2},{3,4,5},{6,7,8},
-                          {0,3,6},{1,4,7},{2,5,8},
-                          {0,4,8},{2,4,6}};
-
-    public void playerTap(View view)
-    {
-        if(activePlayer==-1){
-            dialog.show();
-        }
-        ImageView img;              // here ImageView is the type of variable img
-        img = (ImageView) view;
-        int tappedImage = Integer.parseInt(img.getTag().toString());
-        if(!gameActive){
-            gameReset();
-        }
-        else
-        {
-            if (gameState[tappedImage] == 2 && gameActive) {
-                gameState[tappedImage] = activePlayer;
-                if (activePlayer == 0) {
-                    img.setImageResource(R.drawable.x);
-                    activePlayer = 1;
-                    TextView status = findViewById(R.id.status);
-                    status.setText("O's Turn-Tap on appropriate box");
-                } else {
-                    img.setImageResource(R.drawable.o);
-                    activePlayer = 0;
-                    TextView status = findViewById(R.id.status);
-                    status.setText("X's Turn-Tap on appropriate box");
-                }
-            }
-
-            // draw check
-            boolean draw=true;
-            for (int k:gameState)
-            {
-                if(k==2) {
-                    draw=false;
-                    break;
-                }
-            }
-            if(draw)
-            {
-                TextView status = findViewById(R.id.status);
-                status.setText("Game Ended in a Draw !!");
-                gameActive=false;
-            }
-
-            // win check
-            for (int[] winPosition : winPositions) {
-                if (gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]] && gameState[winPosition[0]] != 2) {
-                    if (gameState[winPosition[0]] == 0) {
-                        TextView status = findViewById(R.id.status);
-                        status.setText("X won !! Game Over");
-                    } else {
-                        TextView status = findViewById(R.id.status);
-                        status.setText("O won !! Game Over");
-                    }
-                    gameActive = false;
-                }
-            }
-        }
-    }
-    public void gameReset()
-    {
-        dialog.show();
-        gameActive=true;
-
-        activePlayer=-1;
-        Arrays.fill(gameState,2);
-
-        ((ImageView)findViewById(R.id.imageView0)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView1)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView2)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView3)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView4)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView5)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView6)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView7)).setImageResource(0);
-        ((ImageView)findViewById(R.id.imageView8)).setImageResource(0);
-    }
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        dialog = new SelectPlayerDialog(this);
-        dialog.show();
+        playeroneScore = (TextView) findViewById(R.id.playerOneScore);
+        playerTwoScore = (TextView) findViewById(R.id.playerTwoScore);
+        playerStatus = (TextView) findViewById(R.id.playerstatus);
+
+        resetGame = (Button) findViewById(R.id.resetGame);
+
+        for (int i = 0; i < buttons.length; i++) {
+            String buttonID = "btn_" + i;
+            int resourceID = getResources().getIdentifier(buttonID, "id", getPackageName());
+            buttons[i] = (Button) findViewById(resourceID);
+            buttons[i].setOnClickListener(this);
+        }
+        rountCount = 0;
+        playerOneScoreCount = 0;
+        playerTwoScoreCount = 0;
+        activePlayer = true;
+
+
     }
 
-    //To handle the creation of menu for resetting game
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main_activity,menu);
-        return true;
+    public void onClick(View view) {
+        if (!((Button) view).getText().toString().equals("")) {
+            return;
+        }
+        String buttonID = view.getResources().getResourceEntryName(view.getId());
+        int gameStatePointer = Integer.parseInt(buttonID.substring(buttonID.length() - 1, buttonID.length()));
+
+        if (activePlayer) {
+            ((Button) view).setText("X");
+            ((Button) view).setTextColor(Color.parseColor("#FFC34A"));
+            gameState[gameStatePointer] = 0;
+        } else {
+
+            ((Button) view).setText("O");
+            ((Button) view).setTextColor(Color.parseColor("#70FFEA"));
+            gameState[gameStatePointer] = 1;
+        }
+        rountCount++;
+
+        if (checkWinner()) {
+            if (activePlayer) {
+                playerOneScoreCount++;
+                updateplayerScore();
+                Toast.makeText(this, "Player One Won!", Toast.LENGTH_SHORT).show();
+                playagain();
+            } else {
+
+                playerTwoScoreCount++;
+                updateplayerScore();
+                Toast.makeText(this, "Player Two Won!", Toast.LENGTH_SHORT).show();
+                playagain();
+            }
+        } else if (rountCount == 9) {
+            playagain();
+            Toast.makeText(this, "No One Won", Toast.LENGTH_SHORT).show();
+        } else {
+            activePlayer = !activePlayer;
+        }
+if(playerOneScoreCount > playerTwoScoreCount){
+    playerStatus.setText("Player One is Winning!!");
+   }else if(playerTwoScoreCount>playerOneScoreCount){
+    playerStatus.setText("Player Two is Winning!!");
+   }else{
+    playerStatus.setText("");
+   }
+resetGame.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+        playagain();
+        playerOneScoreCount=0;
+        playerTwoScoreCount=0;
+        playerStatus.setText("");
+
+        updateplayerScore();
+    }
+});
     }
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onOptionsItemSelected( MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.menu_reset_game:
-                gameReset();
-                // Toast.makeText(this, "Reset Game Confirmed", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+    public boolean checkWinner() {
+        boolean winnerResult = false;
+        for (int[] winningPosition : winningPositions) {
+            if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                winnerResult = true;
+            }
+        }
+        return winnerResult;
+    }
+
+    public void updateplayerScore() {
+        playeroneScore.setText(Integer.toString(playerOneScoreCount));
+
+        playerTwoScore.setText(Integer.toString(playerTwoScoreCount));
+    }
+
+    public void playagain() {
+        rountCount = 0;
+        activePlayer = true;
+        for (int i = 0; i < buttons.length; i++) {
+            gameState[i] = 2;
+            buttons[i].setText("");
         }
     }
 }
+
